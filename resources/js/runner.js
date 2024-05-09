852b2700077ea0d96d35e353483ca87259d0cae6
Cosmin-Valentin <cosminv.nicola@gmail.com>
2024-05-09 11:14:27 +0300

    Js refactor, crouch functionality and start instructions

--- resources/js/runner.js
+++ resources/js/runner.js
@@ -1,63 +1,98 @@
 const runnerContainer = document.querySelector(".runner-container");
-let counter = 0;
-let fail = false;
 
 if (runnerContainer) {
     const chicken = document.getElementById("chicken");
     const obstacle = document.getElementById("obstacle");
     const prompt = document.getElementById("prompt");
-    const ground = document.getElementById("ground");
     const restart = document.querySelector(".restart");
     const skies = document.querySelectorAll(".sky");
-    let x = 1;
 
-    const lose = setInterval(function () {
+    let counter = 0;
+    let fail = false;
+    let x = 1;
+    let loseInterval;
+    let circadianInterval;
+
+    giveInfo();
+
+    function init() {
+        loseInterval = setupLoseCheck();
+        circadianInterval = setupCircadianShift();
+
+        document.addEventListener("keydown", handleJump);
+        restart.addEventListener("click", () => location.reload());
+    }
+
+    function setupLoseCheck() {
+        return setInterval(() => {
+            if (checkCollision(chicken, obstacle)) {
+                gameOver();
+            }
+        }, 10);
+    }
+
+    function checkCollision(chicken, obstacle) {
         let chickenTop = parseInt(
             window.getComputedStyle(chicken).getPropertyValue("top")
         );
         let blockLeft = parseInt(
             window.getComputedStyle(obstacle).getPropertyValue("left")
         );
-        if (blockLeft < 160 && blockLeft > 50 && chickenTop >= 330) {
-            obstacle.style.animation = "none";
-            obstacle.style.display = "none";
-            chicken.style.transform = "rotate(0deg)";
-            chicken.querySelector(".leg").style.animation = "none";
-            ground.querySelector(".pixels").style.animation = "none";
-            ground.querySelector(".pixels2").style.animation = "none";
-            prompt.classList.remove("hidden");
-            prompt.classList.add("flex");
-            fail = true;
-            clearInterval(circadian);
-        }
-    }, 10);
-
-    const circadian = setInterval(function () {
-        skies.forEach((sky) => {
-            sky.classList.add("hidden");
-        });
-        skies[x].classList.remove("hidden");
-        x < 2 ? x++ : (x = 0);
-    }, 15000);
-
-    window.addEventListener("keydown", (e) => {
-        if (e.key === " ") {
-            jump();
-        }
-    });
-
-    restart.addEventListener("click", () => {
-        location.reload();
-    });
-}
-
-function jump() {
-    if (chicken.classList != "animate") {
-        chicken.classList.add("animate");
+        return blockLeft < 160 && blockLeft > 50 && chickenTop >= 330;
     }
 
-    setTimeout(function () {
-        chicken.classList.remove("animate");
+    function gameOver() {
+        if (runnerContainer.classList.contains("move")) {
+            runnerContainer.classList.remove("move");
+            obstacle.classList.add("hidden");
+        }
+        if (chicken.classList.contains("walk")) {
+            chicken.classList.remove("walk");
+        }
+        prompt.classList.remove("hidden");
+        prompt.classList.add("flex");
+        fail = true;
+        clearInterval(loseInterval);
+        clearInterval(circadianInterval);
+        document.removeEventListener("keydown", handleJump);
+    }
+
+    function setupCircadianShift() {
+        return setInterval(() => {
+            skies.forEach((sky) => sky.classList.add("hidden"));
+            skies[x].classList.remove("hidden");
+            x = x < 2 ? x + 1 : 0;
+        }, 15000);
+    }
+
+    function handleJump(e) {
+        const actions = {
+            ArrowUp: () => performAction("jump"),
+            ArrowDown: () => performAction("crouch"),
+        };
+
+        if (actions[e.key]) {
+            actions[e.key]();
+        }
+    }
+
+    function performAction(actionName) {
+        if (!chicken.classList.contains(actionName)) {
+            if (chicken.classList.contains("walk")) {
+                chicken.classList.remove("walk");
+            }
+            chicken.classList.add(actionName);
+            setTimeout(() => {
+                chicken.classList.remove(actionName);
+                if (!fail) {
+                    chicken.classList.add("walk");
+                }
+                checkScoreIncrement();
+            }, 500);
+        }
+    }
+
+    function checkScoreIncrement() {
         let blockLeft = parseInt(
             window.getComputedStyle(obstacle).getPropertyValue("left")
         );
@@ -65,5 +100,26 @@ function jump() {
             counter++;
             document.querySelector(".score b").textContent = counter;
         }
-    }, 500);
+    }
+
+    function startGame(e) {
+        const actionMap = {
+            ArrowUp: "jump",
+            ArrowDown: "crouch",
+        };
+
+        if (actionMap[e.key]) {
+            performAction(actionMap[e.key]);
+            init();
+            document.removeEventListener("keydown", startGame);
+            document.querySelector(".info").classList.add("hidden");
+            if (!runnerContainer.classList.contains("move")) {
+                runnerContainer.classList.add("move");
+            }
+        }
+    }
+
+    function giveInfo() {
+        document.addEventListener("keydown", startGame);
+    }
 }
--- resources/views/games/runner.blade.php
+++ resources/views/games/runner.blade.php
@@ -8,7 +8,7 @@
             <span class="title">Score: </span>
             <b>0</b>
         </div>
-        <p class="info">Press SPACE to jump</p>
+        <p class="info">Press KEY UP or KEY DOWN to start</p>
         <div id="game">
             <img class='sky' src="{{ asset('images/sky.jpg') }}" alt="pixel sky" style="opacity:1">
             <img class='sky hidden' src="{{ asset('images/sky2.jpg') }}" alt="pixel sky2">
@@ -23,14 +23,14 @@
         </div>
         <div id="ground">
             <img class='ground' src="{{ asset('images/ground.png') }}" alt="ground">
-            <div class="pixels">
+            <div class="pixels group-1">
                 <div class="pix1"></div>
                 <div class="pix2"></div>
                 <div class="pix3"></div>
                 <div class="pix4"></div>
                 <div class="pix5"></div>
             </div>
-            <div class="pixels2">
+            <div class="pixels group-2">
                 <div class="pix1"></div>
                 <div class="pix2"></div>
                 <div class="pix3"></div>
